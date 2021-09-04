cd0fec99252833d87bf868d3676bae35bda78329
Danila Fedorin <danila.fedorin@gmail.com>
2021-09-04 16:11:31 -0700

    Add type annotations to BaseUpdateView

--- src/BaseUpdateView.ts
+++ src/BaseUpdateView.ts
@@ -14,40 +14,57 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-export class BaseUpdateView {
-    constructor(value) {
+export interface IObservableValue {
+    on?(event: "change", handler: (props: string[]) => void): void;
+    off?(event: "change", handler: (props: string[]) => void): void;
+}
+
+export interface IMountOptions {
+    parentProvidesUpdates?: boolean
+}
+
+export type MountElement = Element | Comment
+
+export abstract class BaseUpdateView<T extends IObservableValue> {
+    protected _value: T
+    protected _boundUpdateFromValue: ((props: string[]) => void) | null
+
+    abstract update(value: T, props: string[]): void;
+
+    constructor(value :T) {
         this._value = value;
         // TODO: can avoid this if we adopt the handleEvent pattern in our EventListener
         this._boundUpdateFromValue = null;
     }
 
-    mount(options) {
+    mount(options?: IMountOptions): MountElement | null {
         const parentProvidesUpdates = options && options.parentProvidesUpdates;
         if (!parentProvidesUpdates) {
             this._subscribe();
         }
+        return null;
     }
 
-    unmount() {
+    unmount(): void {
         this._unsubscribe();
     }
 
-    get value() {
+    get value(): T {
         return this._value;
     }
 
-    _updateFromValue(changedProps) {
+    _updateFromValue(changedProps: string[]) {
         this.update(this._value, changedProps);
     }
 
-    _subscribe() {
+    _subscribe(): void {
         if (typeof this._value?.on === "function") {
-            this._boundUpdateFromValue = this._updateFromValue.bind(this);
+            this._boundUpdateFromValue = this._updateFromValue.bind(this) as (props: string[]) => void;
             this._value.on("change", this._boundUpdateFromValue);
         }
     }
 
-    _unsubscribe() {
+    _unsubscribe(): void {
         if (this._boundUpdateFromValue) {
             if (typeof this._value.off === "function") {
                 this._value.off("change", this._boundUpdateFromValue);
