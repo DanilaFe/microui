d96db0771f99a50b4d0d845d2c96bca83ed8b8d2
Danila Fedorin <danila.fedorin@gmail.com>
2021-09-04 20:32:19 -0700

    Add an index file

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export {EventEmitter} from "./EventEmitter";
+export {Builder, RenderFn, AttrValue, Attributes, TemplateView, TemplateBuilder} from "./TemplateView";
+export {IObservableValue, IMountOptions, MountElement, BaseUpdateView} from "./BaseUpdateView";
+export {text, HTML_NS, SVG_NS, TAG_NAMES, tag} from "./html";
