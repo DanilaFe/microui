57914d20715d1fca5b4a5bafb8cae83a89b7c222
Danila Fedorin <danila.fedorin@gmail.com>
2021-09-25 15:27:39 -0700

    Port BaseObservable to TypeScript

--- src/observable/BaseObservable.ts
+++ src/observable/BaseObservable.ts
@@ -1,5 +1,6 @@
 /*
 Copyright 2020 Bruno Windels <bruno@windels.cloud>
+Copyright 2021 Daniel Fedorin <danila.fedorin@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +15,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-export class BaseObservable {
+export class BaseObservable<T> {
+    private _handlers: Set<T>;
+
     constructor() {
         this._handlers = new Set();
     }
@@ -27,7 +30,7 @@ export class BaseObservable {
 
     }
 
-    subscribe(handler) {
+    subscribe(handler: T): () => void {
         this._handlers.add(handler);
         if (this._handlers.size === 1) {
             this.onSubscribeFirst();
@@ -37,7 +40,7 @@ export class BaseObservable {
         };
     }
 
-    unsubscribe(handler) {
+    unsubscribe(handler: T | null): void {
         if (handler) {
             this._handlers.delete(handler);
             if (this._handlers.size === 0) {
@@ -45,17 +48,16 @@ export class BaseObservable {
             }
             handler = null;
         }
-        return null;
     }
 
-    unsubscribeAll() {
+    unsubscribeAll(): void {
         if (this._handlers.size !== 0) {
             this._handlers.clear();
             this.onUnsubscribeLast();
         }
     }
 
-    get hasSubscriptions() {
+    get hasSubscriptions(): boolean {
         return this._handlers.size !== 0;
     }
 
@@ -63,11 +65,12 @@ export class BaseObservable {
 }
 
 export function tests() {
-    class Collection extends BaseObservable {
+    class Collection extends BaseObservable<{}> {
+        firstSubscribeCalls = 0;
+        firstUnsubscribeCalls = 0;
+
         constructor() {
             super();
-            this.firstSubscribeCalls = 0;
-            this.firstUnsubscribeCalls = 0;
         }
         onSubscribeFirst() {  this.firstSubscribeCalls += 1; }
         onUnsubscribeLast() { this.firstUnsubscribeCalls += 1; }
