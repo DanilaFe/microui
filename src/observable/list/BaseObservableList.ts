c033ad43d439d96fed7f086fc1c555ef278298fe
Danila Fedorin <danila.fedorin@gmail.com>
2021-09-25 22:58:30 -0700

    Move list-related files into their own folder

--- src/observable/list/BaseObservableList.ts
+++ src/observable/list/BaseObservableList.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import {BaseObservable} from "./BaseObservable";
+import {BaseObservable} from "../BaseObservable";
 
 export interface IObservableListHandler<T> {
     onReset(list: BaseObservableList<T>): void;
